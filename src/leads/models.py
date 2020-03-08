61db7825c645fbe39cb17fcc52d433a6636958ca
ruzny <ruzny@ruzny.com>
2020-03-08 17:46:43 +0530

    lead model setup & added some leads

--- src/leads/models.py
+++ src/leads/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Lead(models.Model):
+    name        = models.CharField(max_length=100)
+    email       = models.EmailField(max_length=120, unique=True)
+    country     = models.CharField(max_length=100)
+    message     = models.TextField(blank=True)
+    created_at  = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
